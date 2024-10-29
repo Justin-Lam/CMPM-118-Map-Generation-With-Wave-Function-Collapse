0eceea75d54c4376f2c41e00b1b770e5491edf71
blytheSchen <blschen@ucsc.edu>
2024-10-29 15:15:09 -0700

    created main loop, outlined functions for constraint solving

--- src/scenes/blythe.js
+++ src/scenes/blythe.js
@@ -11,6 +11,58 @@ class Blythe extends Phaser.Scene
 
 	create()
 	{
+		// temporary weight array, actual weights will be based on pattern frequency when finding them
+		const weights = [
+			1, 1, 1,
+			1, 1, 1,
+			1, 1, 1
+		];
+
+		const adjacencies = [
+			(0, 1, 0),
+		]
+
+		this.failedAttempts = 0;
+		this.isSolved = false;
+
+		this.constraintSolver();
+	}
+
+	constraintSolver()
+	{
+		while (!this.isSolved)
+		{
+			if (this.failedAttempts < MAX_ATTEMPTS)
+			{
+				throw new Error("FAILURE: max attempts reached");
+			}
+
+			this.observe();
+			this.propagate();
+		}
+	}
+
+	clear()
+	{
+
+	}
+
+	ban()
+	{
+
+	}
+
+	observe()
+	{
+
+	}
+
+	propagate()
+	{
+
+	}
+	render()
+	{
 
 	}
 }
