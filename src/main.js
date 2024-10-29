6b7d58cdf85915f0185fb308dddb331031bf0064
Justin-Lam <justinflam9225k@gmail.com>
2024-10-28 21:22:36 -0700

    Added the tile IDs for sand

--- src/main.js
+++ src/main.js
@@ -2,38 +2,50 @@
 "use strict"
 
 // Contants:
-const MAP_WIDTH = 20;
-const TILE_WIDTH = 64;
+const OUTPUT_MAP_WIDTH = 20;		// (of a map) width in tiles
+const TILE_WIDTH = 64;				// (of a tile) width in pixels
 
 // Tile IDs
-// BR = "bottom right", LM = "left middle", TL = "top left", etc.
-const BLANK_TID = 195;		// blank
-const WATER_TID = 56;		// water
-const GRASS_TID = 40;		// grass
-const GRASS_BR_TID = 11;
-const GRASS_BM_TID = 25;
-const GRASS_BL_TID = 39;
-const GRASS_TR_TID = 41;
-const GRASS_TM_TID = 55;
-const GRASS_TL_TID = 69;
-const GRASS_RM_TID = 26;
-const GRASS_LM_TID = 54;
-const DIRT_TID = 175;		// dirt
-const DIRT_BR_TID = 146;
-const DIRT_BM_TID = 160;
-const DIRT_BL_TID = 174;
-const DIRT_TR_TID = 176;
-const DIRT_TM_TID = 190;
-const DIRT_TL_TID = 9;
-const DIRT_RM_TID = 161;
-const DIRT_LM_TID = 189;
+// C = "center", BR = "bottom right", LM = "left middle", TL = "top left", etc.
+const BLANK = 195;		// blank
+const WATER = 56;		// water
+
+const GRASS_C = 40;
+const GRASS_BR = 11;
+const GRASS_BM = 25;
+const GRASS_BL = 39;
+const GRASS_TR = 41;
+const GRASS_TM = 55;
+const GRASS_TL = 69;
+const GRASS_RM = 26;
+const GRASS_LM = 54;
+
+const SAND_C = 110;
+const SAND_BR = 81;
+const SAND_BM = 95;
+const SAND_BL = 109;
+const SAND_TR = 14;
+const SAND_TM = 28;
+const SAND_TL = 42;
+const SAND_RM = 96;
+const SAND_LM = 124;
+
+const DIRT_C = 175;
+const DIRT_BR = 146;
+const DIRT_BM = 160;
+const DIRT_BL = 174;
+const DIRT_TR = 176;
+const DIRT_TM = 190;
+const DIRT_TL = 9;
+const DIRT_RM = 161;
+const DIRT_LM = 189;
 
 // Game Config:
 let config = {
 	parent: "phaser-game",
 	type: Phaser.CANVAS,
-	width: MAP_WIDTH * TILE_WIDTH,
-	height: MAP_WIDTH * TILE_WIDTH,
+	width: OUTPUT_MAP_WIDTH * TILE_WIDTH,
+	height: OUTPUT_MAP_WIDTH * TILE_WIDTH,
 	zoom: 0.5,
 	autoCenter: true,
 	render: {
--- src/scenes/justin.js
+++ src/scenes/justin.js
@@ -6,11 +6,36 @@ class Justin extends Phaser.Scene
 
 	preload()
 	{
-
+		this.load.path = './assets/';
+		this.load.image("map pack", "mapPack_spritesheet.png");
 	}
 
 	create()
 	{
-        console.log("in justin");
+		const inputImage = [
+			[WATER,		WATER,		WATER],
+			[SAND_C,	SAND_C,		WATER],
+			[GRASS_C,	GRASS_C,	SAND_C]
+		];
+
+		const grassTest = [
+			[GRASS_TL,	GRASS_TM,	GRASS_TR],
+			[GRASS_LM,	GRASS_C,	GRASS_RM],
+			[GRASS_BL,	GRASS_BM,	GRASS_BR]
+		];
+
+		const sandTest = [
+			[SAND_TL,	SAND_TM,	SAND_TR],
+			[SAND_LM,	SAND_C,		SAND_RM],
+			[SAND_BL,	SAND_BM,	SAND_BR]
+		];
+
+		const map = this.make.tilemap({
+			data: inputImage,
+			tileWidth: TILE_WIDTH,
+			tileHeight: TILE_WIDTH
+		});
+		const tileset = map.addTilesetImage("map pack");
+		const layer = map.createLayer(0, tileset, 0, 0);
 	}
 }
