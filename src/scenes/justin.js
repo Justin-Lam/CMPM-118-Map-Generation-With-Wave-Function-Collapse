4d06eb2e0b9bf42860a239b523806a46111e1689
Justin-Lam <justinflam9225k@gmail.com>
2024-10-28 21:58:54 -0700

    Code makes the proper amount of NxN patterns

--- src/scenes/justin.js
+++ src/scenes/justin.js
@@ -12,30 +12,63 @@ class Justin extends Phaser.Scene
 
 	create()
 	{
-		const inputImage = [
+		const inputImageMatrix = [
 			[WATER,		WATER,		WATER],
 			[SAND_C,	SAND_C,		WATER],
 			[GRASS_C,	GRASS_C,	SAND_C]
 		];
 
-		const grassTest = [
-			[GRASS_TL,	GRASS_TM,	GRASS_TR],
-			[GRASS_LM,	GRASS_C,	GRASS_RM],
-			[GRASS_BL,	GRASS_BM,	GRASS_BR]
-		];
-
-		const sandTest = [
-			[SAND_TL,	SAND_TM,	SAND_TR],
-			[SAND_LM,	SAND_C,		SAND_RM],
-			[SAND_BL,	SAND_BM,	SAND_BR]
-		];
-
 		const map = this.make.tilemap({
-			data: inputImage,
+			data: inputImageMatrix,
 			tileWidth: TILE_WIDTH,
 			tileHeight: TILE_WIDTH
 		});
 		const tileset = map.addTilesetImage("map pack");
 		const layer = map.createLayer(0, tileset, 0, 0);
+
+		this.debugKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+		this.debugKey.on("down", (key, event) => {
+			// action
+		});
+
+
+		this.processInput(inputImageMatrix, 2);
+	}
+
+	// patternWidth = N (as in NxN)
+	// populates necessary data structures for wfc core
+	processInput(inputImageMatrix, patternWidth)
+	{
+		this.patterns = [];
+
+		const inputWidth = inputImageMatrix.length;
+		for (let y = 0; y < inputWidth; y++) {
+			for (let x = 0; x < inputWidth; x++) {
+
+				const pattern = [];
+				for (let N = 0; N < patternWidth; N++) {
+					pattern[N] = [];
+					for (let N2 = 0; N2 < patternWidth; N2++) {
+						pattern[N][N2] = 1;
+					}
+				}
+				console.log(pattern);
+
+
+			}
+		}
 	}
 }
+
+/*
+potential design for pattern data structure?
+let pattern = {
+	num: 0,
+	tiles: [
+		[],
+		[]
+		// can be 2 or 3 or even more
+	],
+	weight: 1
+}
+*/
